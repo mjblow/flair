9c58ea901abd8687f0577c24d4faccb333bdce7a
Cameron Soulette <csoulette@mustard.prism>
2019-07-02 14:25:29 -0700

    ulimit err fix

--- bin/ssCorrect.py
+++ bin/ssCorrect.py
@@ -357,13 +357,15 @@ def main():
                     #if verbose: tqdm.write("Reference sequence not found in annotations, skipping: %s" % (chrom), file=sys.stderr)
                     continue
             else:
-                with open(os.path.join(tempDir,"%s_temp_reads.bed" % chrom)) as fout:
-                #if chrom not in outDict:
-                    if chrom not in outDict:
+                if chrom not in outDict:
                         readDict[chrom] = os.path.join(tempDir,"%s_temp_reads.bed" % chrom)
+                with open(readDict[chrom],"a+") as fout:
+                #if chrom not in outDict:
+                    
                 #    outDict[chrom] = open(os.path.join(tempDir,"%s_temp_reads.bed" % chrom),'w')
                 #print(line.rstrip(),file=outDict[chrom])
                     print(line.rstrip(),file=fout)
+                    print(line.rstrip(),readDict[chrom])
 
     cmds = list()
     for chrom in readDict:
